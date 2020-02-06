40ac638ac10e16da529d61ae8d6745eb00519830
Michael Elsdörfer <michael@elsdoerfer.info>
2020-02-06 12:53:10 +0000

    Add tests for rtti and refcount.

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -46,6 +46,58 @@ def test_return_an_object(from_code):
     assert foo.bar == 42
 
 
+class TestRTTI:
+
+    def test_get_type_from_pointer(self, from_code):
+        module = from_code("""
+        export class Foo { constructor() {} }
+        export class Bar extends Foo { constructor() { super(); } }        
+        """)
+        foo_type = module.get_type_of(module.Foo())
+        bar_type = module.get_type_of(module.Bar())
+        assert bar_type.base_id == foo_type.id
+
+
+class TestGarbageCollect:
+
+    def test_manual_retain_calls(self, from_code):
+        module = from_code("""
+        export class Foo { constructor() {} }        
+        """)
+
+        foo = module.Foo()
+        foo_pointer = module.get_pointer(foo)
+
+        assert module.get_refcount_of(foo_pointer) == 1
+        module.retain(foo_pointer)
+        assert module.get_refcount_of(foo_pointer) == 2
+        module.release(foo_pointer)
+        assert module.get_refcount_of(foo_pointer) == 1
+
+    def test_release_on_del(self, from_code):
+        module = from_code("""
+        export class Foo { constructor(public x: i32) {} }        
+        export function collect(): void { gc.collect() }
+        """)
+
+        foo = module.Foo(5)
+        foo_pointer = module.get_pointer(foo)
+        assert module.get_refcount_of(foo_pointer) == 1
+
+        # Trigger a delete
+        del foo
+
+        # Note: This seems to be the expected behaviour. The memory gets marked as cleared, but
+        # the compiler does not waste effort to set the ref count to 0.
+        assert module.get_refcount_of(foo_pointer) == 1
+
+        # However, by creating a new object we can verify that it will be put in the same place
+        # as the now deleted on.
+        bar = module.Foo(9)
+        bar_pointer = module.get_pointer(bar)
+        assert bar_pointer == foo_pointer
+
+
 def test_pass_objects_as_arguments(from_code):
     module = from_code("""
     export class Line {
